01bd31870632d49ee68da707ee5fef964c8e168e
KavyaPrabha <59113297+KavyaPrabha@users.noreply.github.com>
2023-03-31 12:55:36 +0530

    Add files via upload
    railway app configurations

--- ShopFinder/ShopFinder/settings.py
+++ ShopFinder/ShopFinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)r#_tpu3v47kl=!9&cu*ifn_b2ak403^4h!4!0tqtqve1(gnpn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
